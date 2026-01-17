8f0de3350f64eedb04749bca150779babf742527
白鸟青城 <1564430143@qq.com>
2026-01-17 14:38:23 +0800

    chore: 更新版本号至 0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/xxxxxfpx/123PanOpen'
 EMAIL = '1564430143@qq.com'
 AUTHOR = 'xxxxxfpx'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = '0.2.5'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
